709786a67ef8627e4eb019d37bcd2c2e5b7e52b3
Norman Saez <norman.saez@alma.cl>
2019-07-02 11:33:01 -0400

    all again

--- check.py
+++ check.py
@@ -1,26 +1,43 @@
-from influxdb import InfluxDBClient
+#from influxdb import InfluxDBClient
 from time import sleep
 #from threading import Thread
 from concurrent.futures import ThreadPoolExecutor
 
 
-def query_count(monitor='Sealand2/FF2/Biofiltros/Biofiltro1/CO2/CO2'):
-    client = InfluxDBClient('localhost', 8086, 'root', 'root', 'blumar')
-    result = client.query('SELECT count("value") FROM "%s"'%monitor)
+def
+query_count(monitor='Sealand2/FF2/Biofiltros/Biofiltro1/CO2/CO2',database='blumar'):
+query_count(monitor='Sealand2/FF2/Biofiltros/Biofiltro1/CO2/CO2',da    print monitor
+query_count(monitor='Sealand2/FF2/Biofiltros/Biofiltro1/CO2/CO2',da    client = InfluxDBClient('localhost', 8086, 'root', 'root', database)
+query_count(monitor='Sealand2/FF2/Biofiltros/Biofiltro1/CO2/CO2',da    result = client.query('SELECT count("value") FROM "%s"'%monitor)
     return list(result.get_points())[0]['count']
+#    return 1 
+    
+def is_stuck(monitor1='Sealand2/FF2/Biofiltros/Biofiltro1/CO2/CO2', monitor2='Sealand2/FF2/Biofiltros/Biofiltro1/CO2/CO2'):
+#    print monitor
+    amount1_1 = query_count(monitor1) 
+    amount2_1 = query_count(monitor2) 
+#    print amount_1
+    sleep(1)
+    amount1_2 = query_count(monitor1)
+    amount2_2 = query_count(monitor2) 
+#    print amount_2
+    diff1 = abs(amount1_2 - amount1_1)
+    diff2 = abs(amount2_2 - amount2_1)
+    
 
-def is_stuck(monitor='Sealand2/FF2/Biofiltros/Biofiltro1/CO2/CO2'):
-    print monitor
-    amount_1 = query_count(monitor) 
-    print amount_1
-    sleep(10)
-    amount_2 = query_count(monitor)
-    print amount_2
-    diff = abs(amount_2 - amount_1)
-    if diff == 0:
-        return True
-    return False
+    if diff1 == 0:
+        stuck1 = True
+    else:
+        stuck1 =False
 
+    if diff2 == 0:
+        stuck2 = True
+    else:
+        stuck2 =False
+
+    return (stuck1 and stuck2)
+        
+        
 def monitor_points():
     '''
     '''
@@ -38,4 +55,4 @@ def monitor_points():
         task = executor.submit(is_stuck,(points))
 if '__main__' == __name__:
     monitor_points()
-    
+   

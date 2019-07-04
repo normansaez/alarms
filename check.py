dddc0231618cd9b7857ad0563a031ac337df763c
Norman Saez <norman.saez@alma.cl>
2019-07-03 20:14:56 -0400

    adding email

--- check.py
+++ check.py
@@ -1,47 +1,66 @@
 #from influxdb import InfluxDBClient
+import smtplib
 from time import sleep
 #from threading import Thread
 from concurrent.futures import ThreadPoolExecutor
 
-def query_count(monitor='Sealand2/FF2/Biofiltros/Biofiltro1/CO2/CO2',database='blumar'):
-    print monitor
-    return None
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+
+def send_email(send_to='norman.saez@blueshadows.cl', room=''):
+    msg = MIMEMultipart()
+    msg['From'] = 'sealand.alarms@gmail.com'
+    msg['To'] = send_to
+    msg['Subject'] = 'Alarm to %s' % room
+    message = 'here is the email'
+    msg.attach(MIMEText(message))
+    
+    mailserver = smtplib.SMTP('smtp.gmail.com:587')
+    # identify ourselves to smtp gmail client
+    mailserver.ehlo()
+    # secure our email with tls encryption
+    mailserver.starttls()
+    # re-identify ourselves as an encrypted connection
+    mailserver.ehlo()
+    mailserver.login('sealand.alarms@gmail.com', 'myalarms123!')
+    
+    mailserver.sendmail('sealand.alarms@gmail.com',send_to, msg.as_string())
+    
+    mailserver.quit()
+
+
+def query_count(database='bluemar', monitor='Sealand2/FF2/Biofiltros/Biofiltro1/CO2/CO2'):
+    return 0
 #    client = InfluxDBClient('localhost', 8086, 'root', 'root', database)
 #    result = client.query('SELECT count("value") FROM "%s"'%monitor)
 #    return list(result.get_points())[0]['count']
     
-def is_stuck(monitors):
-#    print monitors
-#   print len(monitors)
-
-    for i in range(0,len(monitors)+1,2):
-#        print i
-#        try:
-         print monitors[i].split(":")[1]
-         print monitors[i+1].split(":")[1]
-         m1 = query_count(monitors[i].split(":")[1],monitors[i].split(":")[2]) 
-         print m1
-         mi = query_count(monitor[i+1].split(":")[1],monitor[i+1].split(":")[2]) 
-         sleep(30)
-         m2 = query_count(monitors[i].split(":")[1],monitors[i].split(":")[2]) 
-         mii = query_count(monitor[i+1].split(":")[1],monitor[i+1].split(":")[2]) 
-         diff1 = abs(m2 - m1)
-         diff2 = abs(mii - mi)
-#         print diff1 
-         if diff1 == 0:
-             stuck1 = True
-         else:
-             stuck1 =False
-
-         if diff2 == 0:
-             stuck2 = True
-         else:
-             stuck2 =False
-#         print diff1    
-#        except IndexError:
-#            return False
-    return (stuck1 and stuck2)
+def is_stuck(database, point):
+#    print point
+    m1 = query_count(database, point) 
+    sleep(1)
+    m2 = query_count(database, point) 
+    diff = abs(m2-m1)
+    if diff == 0:
+        return True
+    return False
         
+def is_room_stuck(database_room, monitors):
+    print database_room
+    database = database_room.split("|")[0]
+    executor = ThreadPoolExecutor(max_workers=len(monitors))
+    tasks_results = []
+    for point in monitors:
+        print point
+        task = executor.submit(is_stuck,database, point)
+        tasks_results.append(task.result())
+    executor.shutdown(wait=True)
+    print tasks_results
+    result = 0
+    for i in tasks_results:
+        result = i + result
+    print i
+
         
 def monitor_points():
     '''
@@ -49,34 +68,28 @@ def monitor_points():
     filename = 'monitors.ctl'
     fpt = open(filename,'rw')
     lines = fpt.readlines()
-    all_threads = []
+    all_threads = {}
     temp = []
-    d_temp = {}
+    
     for data in lines:
         if data.__contains__('ROOM'):
             if len(temp) != 0:
-                all_threads.append()
+                all_threads.update({database+"|"+room:temp})
                 temp = []
-                d_temp.clear()
         elif not data.__contains__('#'):
-            try:
-                monitor = data.split('Blumar/')[1].rsplit('\n')[0]
-                database = 'blumar'
-            except:
-                monitor = data.split('Chayahue/')[1].rsplit('\n')[0]
-                database = 'chayahue'
-                temp.append(database+":"+monitor)
-            temp.append(database+":"+monitor)
-        
-#        else:
-#            print "data comented"
-#    print all_threads
-#    executor = ThreadPoolExecutor(max_workers=len(all_threads))
-    for points in all_threads:
-#        task = executor.submit(is_stuck,(points))
-        print is_stuck(points)
-#    executor.shutdown()
+                data = data.rsplit('\n')[0]
+                namespace = data.split("/")
+                database = namespace[0]
+                room = namespace[2]
+                monitor = namespace[1]+"/"+namespace[2]+"/"+namespace[3]+"/"+namespace[4]+"/"+namespace[5]+namespace[6]
+                temp.append(monitor)
+    all_threads.update({database+"|"+room:temp})
+
+    executor = ThreadPoolExecutor(max_workers=len(all_threads))
+    for k,v in all_threads.iteritems():
+        is_room_stuck(k,v)
+        task = executor.submit(is_room_stuck,k,v)
+    executor.shutdown(wait=True)
 
 if '__main__' == __name__:
     monitor_points()
-   
